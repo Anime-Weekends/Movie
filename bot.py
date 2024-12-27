a69cad75a757436c4ab31b7d7f4a59b300258e03
rohit3607 <rohitplayer87089@gmail.com>
2024-12-27 22:57:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from plugins import web_server
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
+import asyncio
 from pyromod import listen
 from datetime import datetime
 
@@ -114,15 +115,25 @@ class Bot(Client):
         )
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info("Bot Running..!")
-        self.username = usr_bot_me.username
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @rohit_1888")   
 
-        # web-response
+        # Start Web Server
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+        await web.TCPSite(app, "0.0.0.0", PORT).start()
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
+
+    def run(self):
+        """Run the bot."""
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            self.LOGGER(__name__).info("Shutting down...")
+        finally:
+            loop.run_until_complete(self.stop())
