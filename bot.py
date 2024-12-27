d8af9bfff16e4f72ac7076ef2839741b79e03280
rohit3607 <rohitplayer87089@gmail.com>
2024-12-27 22:03:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,6 +68,37 @@ class Bot(Client):
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
                 sys.exit()
+        if FORCE_SUB_CHANNEL4:
+            try:
+                link = (await self.create_chat_invite_link(chat_id=FORCE_SUB_CHANNEL4, creates_join_request=True)).invite_link
+                self.invitelink4 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(
+                    f"Please Double check the FORCE_SUB_CHANNEL4 value and Make sure Bot is Admin in channel with "
+                    f"Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}"
+                )
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/rohit_1888 for support")
+                sys.exit()
+
+        if FORCE_SUB_CHANNEL3:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL3)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL4)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL3)).invite_link
+                self.invitelink3 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(
+                    f"Please Double check the FORCE_SUB_CHANNEL3 value and Make sure Bot is Admin in channel with "
+                    f"Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}"
+                )
+                self.LOGGER(__name__).info("\nBot Stopped. Dm https://t.me/rohit_1888 for support")
+                sys.exit()
+
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
