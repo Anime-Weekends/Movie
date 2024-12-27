9945c61f760173158ccdcff3ed0ff31aad6be08b
rohit3607 <rohitplayer87089@gmail.com>
2024-12-27 22:21:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import sys
 from pyromod import listen
 from datetime import datetime
 
-from config import ADMINS, API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL,FORCE_SUB_CHANNEL2, CHANNEL_ID, PORT, OWNER_ID
+from config import *
 
 
 # fix for current pyrogram 
