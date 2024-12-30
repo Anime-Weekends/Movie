8133312a67ddfcb9849e78f657f9085b60846c2d
rohit3607 <rohitplayer87089@gmail.com>
2024-12-31 00:58:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -73,7 +73,7 @@ PICS = (os.environ.get("PICS", "https://envs.sh/4Iq.jpg https://envs.sh/4IW.jpg 
 
 #Payment to remove the token system
 #put TRUE if you want this feature
-USE_PAYMENT = True if (True if os.environ.get("USE_PAYMENT", "FALSE") == "TRUE" else False) and (USE_SHORTLINK) else False
+USE_PAYMENT = True if (True if os.environ.get("USE_PAYMENT", "TRUE") == "TRUE" else False) and (USE_SHORTLINK) else False
 #UPI ID
 UPI_ID = os.environ.get("UPI_ID", "clementrajadurai@okhdfcbank")
 #UPI QR CODE IMAGE
