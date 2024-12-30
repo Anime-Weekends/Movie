1055bb0b9dd18001f737c8e6eeba963bc34c34d4
rohit3607 <rohitplayer87089@gmail.com>
2024-12-31 00:36:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -24,6 +24,8 @@ from datetime import datetime, timedelta
 from pytz import timezone
 
 
+CHNL_BTN = None
+
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
