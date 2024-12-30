1a303bcf31b892da2e5c43abb8f0c3594119973d
rohit3607 <rohitplayer87089@gmail.com>
2024-12-30 23:41:30 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -7,7 +7,7 @@ from bot import Bot
 from config import *
 from helper_func import *
 
-@Bot.on_message(filters.private & is_admin & ~filters.command(['start','users','broadcast','batch','genlink','stats','auth_secret','deauth_secret', 'auth', 'sbatch', 'exit', 'add_admins', 'del_admins', 'admin_list', 'add_prem', 'ping', 'restart', 'ch2l', 'cancel', 'req_fsub', 'fsub_chnl', 'add_fsub', 'del_fsub']))
+@Bot.on_message(filters.private & is_admin & ~filters.command(['start','users','broadcast','batch','genlink','stats','auth_secret','deauth_secret', 'auth', 'sbatch', 'exit', 'add_admins', 'del_admins', 'admin_list', 'add_prem', 'ping', 'restart', 'ch2l', 'cancel', 'req_fsub', 'fsub_chnl', 'add_fsub', 'del_fsub', 'auto_del', 'files']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...! 🫷", quote = True)
     try:
