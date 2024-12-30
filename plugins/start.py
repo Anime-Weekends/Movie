afe5444c09e71cc9813dab734b1afdd2c8c23450
rohit3607 <rohitplayer87089@gmail.com>
2024-12-30 22:53:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,8 +12,7 @@ from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import *
 from helper_func import *
-from database.database import add_admin, add_user, del_admin, del_user, full_adminbase, full_userbase, gen_new_count, get_clicks, inc_count, new_link, present_admin, present_hash, present_user
-from database.join_reqs import *
+from database.database import db
 
 SECONDS = TIME 
 TUT_VID = f"{TUT_VID}"
