84cba7a6a530afa0e74b82150ba6c7e8c7d61b22
rohit3607 <rohitplayer87089@gmail.com>
2024-12-30 22:46:37 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -14,6 +14,66 @@ from datetime import datetime
 from database.database import user_data, db_verify_status, db_update_verify_status
 import pytz
 
+# used for checking banned user
+async def check_banUser(filter, client, update):
+    try:
+        user_id = update.from_user.id
+        return await db.ban_user_exist(user_id)
+    except: #Exception as e:
+        #print(f"!Error on check_banUser(): {e}")
+        return False
+
+
+#used for cheking if a user is admin ~Owner also treated as admin level
+async def check_admin(filter, client, update):
+    try:
+        user_id = update.from_user.id       
+        return any([user_id == OWNER_ID, await db.admin_exist(user_id)])
+    except Exception as e:
+        print(f"! Exception in check_admin: {e}")
+        return False
+
+# Check user subscription in Channels in a more optimized way
+async def is_subscribed(filter, client, update):
+    Channel_ids = await db.get_all_channels()
+
+    if not Channel_ids:
+        return True
+
+    user_id = update.from_user.id
+
+    if any([user_id == OWNER_ID, await db.admin_exist(user_id)]):
+        return True
+
+    # Handle the case for a single channel directly (no need for gather)
+    if len(Channel_ids) == 1:
+        return await is_userJoin(client, user_id, Channel_ids[0])
+
+    # Use asyncio gather to check multiple channels concurrently
+    tasks = [is_userJoin(client, user_id, ids) for ids in Channel_ids if ids]
+    results = await asyncio.gather(*tasks)
+
+    # If any result is False, return False; else return True
+    return all(results)
+
+
+#Chcek user subscription by specifying channel id and user id
+async def is_userJoin(client, user_id, channel_id):
+    #REQFSUB = await db.get_request_forcesub()
+    try:
+        member = await client.get_chat_member(chat_id=channel_id, user_id=user_id)
+        return member.status in {ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER}
+
+    except UserNotParticipant:
+        if await db.get_request_forcesub(): #and await privateChannel(client, channel_id):
+                return await db.reqSent_user_exist(channel_id, user_id)
+
+        return False
+
+    except Exception as e:
+        print(f"!Error on is_userJoin(): {e}")
+        return False
+#=============================================================================================================================================================================
 
 async def encode(string):
     string_bytes = string.encode("ascii")
