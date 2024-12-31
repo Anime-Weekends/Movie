eece7f1310fa489fd270b7dd06375fa1c1ce8a51
rohit3607 <rohitplayer87089@gmail.com>
2025-01-01 01:48:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -140,8 +140,9 @@ async def start_command(client: Client, message: Message):
                 [[InlineKeyboardButton("About Me", callback_data="about"),
                   InlineKeyboardButton("Close", callback_data="close")]]
             )
-            await message.reply_text(
-                text=START_MSG.format(
+            await message.reply_photo(
+                photo=START_PIC,
+                caption=START_MSG.format(
                     first=message.from_user.first_name,
                     last=message.from_user.last_name,
                     username=None if not message.from_user.username else '@' + message.from_user.username,
